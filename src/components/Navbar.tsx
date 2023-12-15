255e685eba8884a27259c7c7e2ec4b4582315a41
Andrey Silva <andreysl2001@gmail.com>
2023-12-15 15:43:53 -0300

    Animação do menu adicionada

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,5 @@
+import { useRef } from "react";
+
 // Icons
 import {AiOutlineLogout} from "react-icons/ai"
 import { GiHamburgerMenu } from "react-icons/gi";
@@ -19,12 +21,22 @@ const Navbar = () => {
   let { pathname } = URL;
   const words = ["brand", "car", "login", "about", "register"];
   let formatNavbar = words.some((word) => pathname.includes(word));
+  const nav = useRef<HTMLUListElement>(null)
 
   const [user] = useAuthState(auth); // Caso você logue com outro conta o usuário será automáticamente atualizado
 
   const signUserOut = async () => {
     await signOut(auth);
   };
+
+  // RESPONSIVE MENU
+  const openMenu = () => {
+    nav.current?.classList.add("fadeInMenu")
+  }
+
+  const closeMenu = () => {
+    nav.current?.classList.remove("fadeInMenu")
+  }
 
   return (
     <nav className="navbar" style={formatNavbar ? { background: "white" } : {}}>
@@ -34,10 +46,15 @@ const Navbar = () => {
         </Link>
       </div>
 
-      <GiHamburgerMenu />
+      <span className="menuIcon" onClick={() => openMenu()}>
+        <GiHamburgerMenu />
+      </span>
 
-      <ul>
-      <FaWindowClose />
+      <ul ref={nav}>
+        <span className="closeIcon" onClick={() => closeMenu()}>
+          <FaWindowClose />
+        </span>
+
         <li>
           <NavLink to="/" className={formatNavbar ? "formatedLink" : ""}>
             Página inicial
